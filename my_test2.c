0bb2fddcb76e69268ce1353c5d27105d7948ee1b
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@e0adee21-3a0e-50dc-bfab-84b5028d302a>
2013-03-18 03:35:46 +0000

    add my_test2.c

---
+++ my_test2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define WriteLine() printf("\n");
+#define WriteLong(x) printf(" %lld", x);
+#define ReadLong(a) if (fscanf(stdin, "%lld", &a) != 1) a = 0;
+#define long long long
+
+void main()
+{
+  long a;
+  long b;
+  long c;
+  a = 1;
+  b = 2;
+  c = 3;
+
+  if (a < 70) {
+    while (a < 75){
+      a = a+(b+c);
+    }
+  }
+  a = a+(b+c);
+  WriteLong(a);
+}
